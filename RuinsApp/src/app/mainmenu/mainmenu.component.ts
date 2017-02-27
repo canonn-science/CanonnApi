9aeec93d129aeca2408c726f20447776063e4386
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-02-28 00:32:54 +0100

    Code cleanup

--- RuinsApp/src/app/mainmenu/mainmenu.component.ts
+++ RuinsApp/src/app/mainmenu/mainmenu.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-mainmenu',
-  templateUrl: './mainmenu.component.html',
-  styleUrls: ['./mainmenu.component.less']
+	selector: 'app-mainmenu',
+	templateUrl: './mainmenu.component.html',
+	styleUrls: ['./mainmenu.component.less']
 })
 export class MainmenuComponent implements OnInit {
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
