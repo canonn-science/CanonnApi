2324f18040679d35492685f4975ae1c3dc997e75
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-02-28 00:32:45 +0100

    Made port configurable to be able to run two instances in IIS

--- RuinsApi/RuinsApi/Program.cs
+++ RuinsApi/RuinsApi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace RuinsApi
 {
@@ -7,13 +8,25 @@ namespace RuinsApi
 	{
 		public static void Main(string[] args)
 		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("hosting.json", optional: true)
+				.AddJsonFile("hosting.prod.json", optional: true)
+				.Build();
+
+			int port = 52685;
+			if ((args.Length == 1) && !int.TryParse(args[0], out port))
+			{
+				port = 52685;
+			}
+
 			var host = new WebHostBuilder()
 				 .UseKestrel()
+				 .UseConfiguration(config)
 				 .UseContentRoot(Directory.GetCurrentDirectory())
 				 .UseIISIntegration()
 				 .UseStartup<Startup>()
 				 .UseApplicationInsights()
-				 .UseUrls("http://localhost:52685")
 				 .Build();
 
 			host.Run();
