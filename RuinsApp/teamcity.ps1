01f586046f3fb2ddb3126ea6a0f01d5c5456ab4c
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-03-18 14:13:33 +0100

    Added build and deployment infrastructure

---
+++ RuinsApp/teamcity.ps1
@@ -0,0 +1,4 @@
+$branch = [Environment]::GetEnvironmentVariable("teamcity.build.branch").ToLower()
+$buildCounter = [Environment]::GetEnvironmentVariable("build.counter").ToLower()
+
+Write-Host "##teamcity[buildNumber '1.0.0-$branch-$buildCounter']"
