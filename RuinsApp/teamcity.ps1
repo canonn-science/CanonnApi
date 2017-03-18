56311547c1ad7890d3c5234d06c8d2e83b9275fe
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-03-18 14:45:41 +0100

    Updated variable setting

--- RuinsApp/teamcity.ps1
+++ RuinsApp/teamcity.ps1
@@ -1,6 +1,12 @@
 Get-ChildItem Env:
 
 $branch = [Environment]::GetEnvironmentVariable("teamcity.build.branch").ToLower()
+if ($branch -eq "master") {
+	$branch = "";
+} else {
+	$branch = "-$branch"
+}
+
 $buildCounter = [Environment]::GetEnvironmentVariable("build.counter").ToLower()
 
-Write-Host "##teamcity[buildNumber '1.0.0-$branch-$buildCounter']"
+Write-Host "##teamcity[buildNumber '1.0.0$branch-$buildCounter']"
