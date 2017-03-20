8a529cc47a26a791b3b5912aa5738dffa42aa83b
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-03-20 23:26:36 +0100

    Revert "Prepared new build config"
    This reverts commit 8203b36fd86bb0e3f06ce6abd738e8f25ef664b9.

---
+++ RuinsApi/RuinsApi/teamcity.ps1
@@ -0,0 +1,45 @@
+# clean up dist folder from possible earlier build
+$strFolderName="dist"
+If (Test-Path $strFolderName){
+	Remove-Item $strFolderName
+}
+
+$versionObj = [version](Get-Content version.txt)
+$version = '{0}.{1}.{2}' -f $versionObj.Major, $versionObj.Minor, $versionObj.Build
+
+# set build version based on branch (if set by teamcity)
+$branch = [Environment]::GetEnvironmentVariable("teamcity.build.branch")
+if (!$branch) {
+	$branch = "localdev"
+}
+$branch = $branch.ToLower()
+$buildCounter = [Environment]::GetEnvironmentVariable("build.counter")
+if ($buildCounter) {
+	$buildCounter = "-$buildCounter"
+}
+
+if ($branch -eq "refs/heads/master") {
+	$suffix = "";
+} else {
+	$suffix = "-$branch$buildCounter"
+}
+
+$versionString = "$version$suffix";
+
+# set new version tag to teamcity
+Write-Host "##teamcity[buildNumber '$versionString']"
+
+# write version number to assemblyinfo
+$path = ".\properties\AssemblyInfo.cs"
+(Get-Content $path) | ForEach-Object{
+    if($_ -match '\[assembly: AssemblyVersion\("(.*)"\)\]'){
+        '[assembly: AssemblyVersion("{0}.{1}")]' -f $version, $versionObj.Revision
+    } ElseIf ($_ -match '\[assembly: AssemblyFileVersion\("(.*)"\)\]') {
+        '[assembly: AssemblyFileVersion("{0}")]' -f $version, $versionObj.Build
+    } ElseIf ($_ -match '\[assembly: AssemblyInformationalVersion\("(.*)"\)\]') {
+        '[assembly: AssemblyInformationalVersion("{0}")]' -f $versionString
+    } Else {
+        # Output line as is
+        $_
+    }
+} | Set-Content $path
