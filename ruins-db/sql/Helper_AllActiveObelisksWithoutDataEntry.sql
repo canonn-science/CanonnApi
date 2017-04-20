67b9600d1c3100516eec517d91d65c7d34280585
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-04-20 22:33:26 +0200

    Added helper SQL

---
+++ ruins-db/sql/Helper_AllActiveObelisksWithoutDataEntry.sql
@@ -0,0 +1,18 @@
+SELECT
+	`obelisk`.`id` AS `#`, 
+	`ruin_type`.`name` AS `Type`,
+	`obelisk_group`.`name` AS `Group`,
+	`obelisk`.`number` AS `Number`,
+	`ruin_site`.`id` AS `GS#`,
+	`system`.`name` AS `System`,
+	`body`.`name` AS `Body`
+	FROM `obelisk`
+	JOIN `obelisk_group` ON `obelisk_group`.`id` = `obelisk`.`id`
+	JOIN `ruin_type` ON `ruin_type`.`id` = `obelisk_group`.`ruintype_id`
+	INNER JOIN `ruinsite_activeobelisks` ON `ruinsite_activeobelisks`.`obelisk_id` = `obelisk`.`id`
+	INNER JOIN `ruin_site` ON `ruin_site`.`id` = `ruinsite_activeobelisks`.`ruinsite_id`
+	JOIN `body` ON `body`.`id` = `ruin_site`.`body_id`
+	JOIN `system` ON `system`.`id` = `body`.`system_id`
+	WHERE `obelisk`.`codexdata_id` IS NULL
+		AND `ruinsite_activeobelisks`.`ruinsite_id` < 9997
+	
