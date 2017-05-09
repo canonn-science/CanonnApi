5898011a9cebb4889e1fd059f7f9b637bdb7f824
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-05-09 08:13:45 +0200

    Updated helper queries

--- ruins-db/sql/Helper_AllActiveObelisksWithoutDataEntry.sql
+++ ruins-db/sql/Helper_AllActiveObelisksWithoutDataEntry.sql
@@ -1,11 +1,15 @@
 SELECT
-	`obelisk`.`id` AS `#`, 
+	`obelisk`.`id` AS `#`,
 	`ruin_type`.`name` AS `Type`,
 	`obelisk_group`.`name` AS `Group`,
 	`obelisk`.`number` AS `Number`,
 	`ruin_site`.`id` AS `GS#`,
 	`system`.`name` AS `System`,
-	`body`.`name` AS `Body`
+	`body`.`name` AS `Body`,
+	(SELECT COUNT(*) FROM `obelisk`
+		INNER JOIN `ruinsite_activeobelisks` ON `ruinsite_activeobelisks`.`obelisk_id` = `obelisk`.`id`
+		WHERE `obelisk`.`codexdata_id` IS NULL
+		AND `ruinsite_activeobelisks`.`ruinsite_id` = `ruin_site`.`id`) AS `# on this site`
 	FROM `obelisk`
 	JOIN `obelisk_group` ON `obelisk_group`.`id` = `obelisk`.`obeliskgroup_id`
 	JOIN `ruin_type` ON `ruin_type`.`id` = `obelisk_group`.`ruintype_id`
@@ -15,5 +19,4 @@ SELECT
 	JOIN `system` ON `system`.`id` = `body`.`system_id`
 	WHERE `obelisk`.`codexdata_id` IS NULL
 		AND `ruinsite_activeobelisks`.`ruinsite_id` < 9997
-	ORDER BY `Type`, `Group`, `Number`;
-	
+	ORDER BY `# on this site` DESC, `System`, `Body`, `Type`, `Group`, `Number`;
